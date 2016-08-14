a678ee98a60889bee7ee8eb587e66175e4400d20
James <jedwards@usagb.net>
2016-08-14 14:49:22 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Blogger - James Edwards
+### Authentication included
 
 Deployed here: [http://salty-burn.surge.sh]
+Hosted on Heroku: [https://jedwardsblog2.herokuapp.com/api]
 
 Note: deleting posts from the home page does not cause the page to refresh, but refreshing the page will show that the posts you tried to delete are gone.
--- src/actions/index.js
+++ src/actions/index.js
@@ -13,9 +13,9 @@ export const ActionTypes = {
   AUTH_ERROR: 'AUTH_ERROR',
 };
 
-// const ROOT_URL = 'https://jedwardsblog.herokuapp.com/api';
+const ROOT_URL = 'https://jedwardsblog2.herokuapp.com/api';
 // const ROOT_URL = 'https://cs52-blog.herokuapp.com/api';
-const ROOT_URL = 'http://localhost:9090/api';
+// const ROOT_URL = 'http://localhost:9090/api';
 const API_KEY = '?key=j_edwards';
 
 export function fetchPosts() {
